8294fe2028006b907c4882997b9ca7e722d2d0b4
Dilyara <dilyra.as@gmail.com>
2019-10-23 23:29:33 +0300

    _name

---
+++ Matrix_Homework.java
@@ -0,0 +1,122 @@
+import java.util.Scanner;
+
+public class Matrix_Homework {
+    public static int[][] enter_mass(int m, int n) {
+        int [][] mas = new int [m][n];
+        for (int i = 0; i<m; i++) {
+            for (int j = 0; j<n; j++) {
+                mas [i][j] = (int) (Math.random() * 100);
+                System.out.printf("%4d", mas[i][j]);
+            }
+            System.out.println();
+        }
+        return mas;
+    }
+    public static void matrix20(int [][] mas){
+        int p = 1;
+        for (int i = 0; i<mas[0].length; i++) {
+            for (int j = 0; j<mas.length; j++) {
+                p*=mas[j][i];
+            }
+            System.out.println("Произведение " + i + " "  + "столбцов:" + p);
+            p = 1;
+        }
+    }
+
+    public static void matrix17(int [][] mas, int k){
+        int s = 0;
+        int p = 1;
+        System.out.println();
+
+        for (int j = 0; j<mas[0].length; j++) {
+            s+=mas[k][j];
+            p*=mas[k][j];
+        }
+
+        System.out.println("сумма = " + s);
+        System.out.println("произведение = " + p);
+    }
+
+    public static void matrix27 (int [][] mas) {
+        int min = Integer.MAX_VALUE;
+        int min_max = Integer.MIN_VALUE;
+        for (int i = 0; i <mas.length; i++) {
+            for (int j = 0; j < mas[0].length; j++) {
+                if (mas[i][j] < min) {
+                    min = mas[i][j];
+                }
+            }
+            if (min > min_max) {
+                min_max = min;
+            }
+            min = Integer.MAX_VALUE;
+        }
+        System.out.println("максимальный среди минимальных:"+ min_max);
+    }
+
+    public static void matrix47(int [][] mas, int k, int k2) {
+        int [] mas_k = mas [k];
+        int [] mas_k2 = mas[k2];
+        mas[k] = mas_k2;
+        mas[k2] = mas_k;
+        System.out.println();
+        for (int i = 0; i < mas.length ; i++) {
+            for (int j = 0; j < mas[0].length ; j++) {
+                System.out.printf("%3d", mas[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+     public static void matrix61(int [][] mas, int k) {
+         for (int i = 0; i < mas.length; i++) {
+             if (i == k) continue;
+             for (int j = 0; j < mas[0].length; j++) {
+                     System.out.printf("%3d", mas[i][j]);
+             }
+             System.out.println();
+         }
+         System.out.println();
+    }
+
+    public static void matrix88(int[][] mas) {
+        for (int i = 0; i < mas.length ; i++) {
+            for (int j = 0; j < mas[0].length ; j++) {
+                if (j < i) {
+                    mas[i][j] = 0;
+                }
+            }
+            }
+            for (int i = 0; i < mas.length ; i++) {
+                for (int j = 0; j < mas[0].length ; j++) {
+                    System.out.printf("%3d", mas[i][j]);
+                }
+                System.out.println();
+            }
+
+    }
+
+
+    public static void main(String [] args) {
+        System.out.println("Enter M:");
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        System.out.println("Enter N:");
+        int n = sc.nextInt();
+        System.out.println("Enter K:");
+        int k = sc.nextInt()-1;
+        System.out.println("Enter K2:");
+        int k2 = sc.nextInt()-1;
+        int [][] mas = new int [m][n];
+        mas = enter_mass(m,n);
+        matrix20(mas);
+        matrix17(mas,k);
+        matrix27(mas);
+        matrix47(mas,k,k2);
+        matrix61(mas,k);
+        matrix88(mas);
+
+
+    }
+}
